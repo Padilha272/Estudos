76bcce2bdbe886d9513cb5ed7dcffc4db1a7a09a
Matheus Padilha <matheuspadilha48@hotmail.com>
2023-02-14 11:09:19 -0300

    14/02/2023

--- Aula224-Interfaces/src/application/Program.java
+++ Aula224-Interfaces/src/application/Program.java
@@ -19,6 +19,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
+		System.out.println("----------------Program-----------------");
 		System.out.println("Enter rental data");
 		System.out.print("Car model: ");
 		String carModel = sc.nextLine();
